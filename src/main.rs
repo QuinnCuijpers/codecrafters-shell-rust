eb2debf253f2d8860484255cee3dfa240155b878
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-01-21 23:38:17 +0100

    REPL

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-use anyhow::{Context, Ok};
+use anyhow::{Context};
 
 fn main() -> anyhow::Result<()> {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().context("flushing stdout")?;
-    let mut buf = String::new();
-    let _input = io::stdin().read_line(&mut buf).context("reading stdin")?;
-    let command = buf.trim_end();
-    println!("{command}: command not found");
-    Ok(())
+    loop {
+        print!("$ ");
+        io::stdout().flush().context("flushing stdout")?;
+        let mut buf = String::new();
+        let _input = io::stdin().read_line(&mut buf).context("reading stdin")?;
+        let command = buf.trim_end();
+        println!("{command}: command not found");
+    }
 }
